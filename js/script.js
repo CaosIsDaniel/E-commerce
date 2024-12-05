10eeadc2ce10391a520fdc798fbc93073c92f28f
Margi81 <margara.8121@gmail.com>
2024-12-04 20:22:45 -0500

    script Lila

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Función para aplicar los filtros
+function applyFilters() {
+    const searchText = document.getElementById('search-input').value.toLowerCase();
+    const checkboxes = document.querySelectorAll('.filter-checkbox');
+    const products = document.querySelectorAll('.product');
+
+    const activeFilters = Array.from(checkboxes)
+        .filter(checkbox => checkbox.checked)
+        .map(checkbox => checkbox.value);
+
+    products.forEach(product => {
+        const productName = product.querySelector('p').textContent.toLowerCase();
+        const productCategory = product.classList;
+
+        if (
+            (activeFilters.length === 0 || activeFilters.some(filter => productCategory.contains(filter))) &&
+            (searchText === "" || productName.includes(searchText))
+        ) {
+            product.style.display = 'block';
+        } else {
+            product.style.display = 'none';
+        }
+    });
+}
